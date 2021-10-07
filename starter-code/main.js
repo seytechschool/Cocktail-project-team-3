70206a102f3a4b07522c0efa8a4204f9aac7f070
Aiperi Karabalaeva <karabalaevaiperi@gmail.com>
2021-10-07 11:12:01 -0700

    filter searchInput

--- starter-code/main.js
+++ starter-code/main.js
@@ -7,9 +7,12 @@ import { cocktails } from "./data.js"; // this is your data
 ///////////////////////////////////////////////////////////////
 // let wrapper = document.querySelector('.wrapper');
 let cocktailWrapper = document.querySelector(".cocktail-wrapper");
+let searchButton = document.querySelector(".search-button")
+let searchInput = document.querySelector(".cocktail-search-name")
+
 cocktails.forEach(arr => {
 	let cocktail = `
-    <div class="cocktail">
+        <div class="cocktail">
         <img src="${arr.strDrinkThumb}" />
         <p class="cocktail-name">${arr.strDrink}</p> 
         </div>`;
@@ -17,5 +20,28 @@ cocktails.forEach(arr => {
         div.setAttribute("class", "cocktails-blocks")
         div.innerHTML = cocktail;
         cocktailWrapper.appendChild(div);
-    });
- 
+   });
+
+searchInput.addEventListener("input", searchStrDrink)
+
+function searchStrDrink(){
+   let div = document.querySelectorAll(".cocktails-blocks")
+   div.forEach(el => el.innerHTML = "")
+   let filteredCoctailName = cocktails.filter(el =>{
+      return el.strDrink.toLowerCase().includes(searchInput.value.toLowerCase());
+   })
+   filteredCoctailName.forEach((el,index) =>{
+      let filteredCoctails = `
+      <div class="cocktail">
+      <img src="${el.strDrinkThumb}" />
+      <p class="cocktail-name">${el.strDrink}</p> 
+      </div>`;
+      div[index].innerHTML = filteredCoctails
+   })
+}
+
+
+
+
+
+
